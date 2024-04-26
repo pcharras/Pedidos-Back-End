b97570ce84d3ab801cc98c7e484eaa415f2a1e30
JaviPeder~ <javierpedernera@gmail.com>
2024-04-26 14:28:52 -0300

    cambios zonas

--- run.py
+++ run.py
@@ -9,4 +9,4 @@ app = create_app()
 if __name__ == '__main__':
     # Aquí puedes configurar opciones adicionales para la ejecución del servidor
     # como el puerto, el modo de depuración, etc.
-    app.run(debug=True,host='127.0.0.1',port=8000)  # Cambia debug a False en un entorno de producción
+    app.run(debug=True,host='127.0.0.1',port=8000) # Cambia debug a False en un entorno de producción
