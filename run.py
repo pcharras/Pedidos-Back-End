934ce35a59db4af84e0b3ee36ab7e52fd25d557e
JaviPeder~ <javierpedernera@gmail.com>
2024-04-26 18:46:13 -0300

    Carga de estados

--- run.py
+++ run.py
@@ -1,10 +1,18 @@
-from app import create_app
-
+from app import create_app, db
+from app.models import Estado
 # Aquí puedes configurar el entorno o cualquier otro ajuste previo al lanzamiento
 # Por ejemplo, puedes establecer variables de entorno
 
 app = create_app()
 
+with app.app_context():
+    for estado_nombre in ["Abierto", "Cerrado", "Enviado", "Cancelado", "Pendiente", "Entregado"]:
+        estado_existente = Estado.query.filter_by(nombre=estado_nombre).first()
+        if not estado_existente:
+            nuevo_estado = Estado(nombre=estado_nombre)
+            db.session.add(nuevo_estado)
+            db.session.commit()
+    print('Estados cargados...')
 
 if __name__ == '__main__':
     # Aquí puedes configurar opciones adicionales para la ejecución del servidor
