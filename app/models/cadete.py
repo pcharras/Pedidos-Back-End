6c0befdd940656f13518788c6e3e3ed317f69560
JaviPeder~ <javierpedernera@gmail.com>
2024-04-30 20:04:26 -0300

    actualizo cadetes

--- app/models/cadete.py
+++ app/models/cadete.py
@@ -6,17 +6,20 @@ class Cadete(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(250), nullable=False)
     activo = db.Column(db.Boolean, nullable=False)
+    telefono = db.Column(db.String(20), nullable=True)
 
     def serialize(self):
         return {
             "id": self.id,
             "nombre": self.nombre,
-            "activo": self.activo
+            "activo": self.activo,
+            "telefono": self.telefono
         }
     
-    def __init__(self, nombre, activo):
+    def __init__(self, nombre, activo, telefono):
         self.nombre = nombre
         self.activo = activo
+        self.telefono = telefono
 
     def __repr__(self):
         return f"<Cadete {self.id}: {self.nombre}, {'Activo' if self.activo else 'Inactivo'}>"
--- app/services/cadete_service.py
+++ app/services/cadete_service.py
@@ -7,20 +7,22 @@ class CadeteService:
         return Cadete.query.get(cadete_id)
 
     @staticmethod
-    def create_cadete(nombre, activo):
-        new_cadete = Cadete(nombre=nombre, activo=activo)
+    def create_cadete(nombre, activo, telefono=None):
+        new_cadete = Cadete(nombre=nombre, activo=activo, telefono=telefono)
         db.session.add(new_cadete)
         db.session.commit()
         return new_cadete
 
     @staticmethod
-    def update_cadete(cadete_id, nombre=None, activo=None):
+    def update_cadete(cadete_id, nombre=None, activo=None, telefono=None):
         cadete = CadeteService.get_cadete_by_id(cadete_id)
         if cadete:
             if nombre is not None:
                 cadete.nombre = nombre
             if activo is not None:
                 cadete.activo = activo
+            if telefono is not None:
+                cadete.telefono = telefono
             db.session.commit()
         return cadete
 
