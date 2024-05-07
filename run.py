8e10fae72513f855bb937a1a792aaf2b716a8f01
JaviPeder~ <javierpedernera@gmail.com>
2024-05-07 17:31:26 -0300

    cambios en editar grupo

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 from app import create_app, db
-from app.models import Estado
+from app.models import Estado, Parametro
 # Aquí puedes configurar el entorno o cualquier otro ajuste previo al lanzamiento
 # Por ejemplo, puedes establecer variables de entorno
-
+parametros = [
+    {"nombre": "MaxPedidosPorGrupo", "valor": "5"},
+    {"nombre": "MaxEsperaPorGrupo", "valor": "40"},
+    {"nombre": "Ciudad", "valor": "Carlos Paz"}
+]
 app = create_app()
 
 with app.app_context():
@@ -11,8 +15,15 @@ with app.app_context():
         if not estado_existente:
             nuevo_estado = Estado(nombre=estado_nombre)
             db.session.add(nuevo_estado)
-            db.session.commit()
+            db.session.commit()        
     print('Estados cargados...')
+    for parametro_data in parametros:
+        parametro_existente = Parametro.query.filter_by(nombre=parametro_data["nombre"]).first()
+        if not parametro_existente:
+            nuevo_parametro = Parametro(nombre=parametro_data["nombre"], valor=parametro_data["valor"])
+            db.session.add(nuevo_parametro)
+            db.session.commit()
+    print('Parámetros cargados...')
 
 if __name__ == '__main__':
     # Aquí puedes configurar opciones adicionales para la ejecución del servidor
